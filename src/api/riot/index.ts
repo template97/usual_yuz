75f25e0bf94d1ed08ff2c5cb1c36aec00abe4c7e
usualyuz <gogodbwngud@gmail.com>
2022-06-11 16:59:20 +0900

    feat: Set riot api

--- src/api/riot/index.ts
+++ src/api/riot/index.ts
@@ -3,8 +3,46 @@ import envConfig from 'src/config';
 import { axiosRequest } from '..';
 import { SummonerDto } from './interface';
 
-const RIOT_API_URL = 'https://kr.api.riotgames.com';
-
-export const GET_summoner_by_name = (name: string): Promise<AxiosResponse<SummonerDto>> => {
-  return axiosRequest('GET', `${RIOT_API_URL}/lol/summoner/v4/summoners/by-name/${name}?api_key=${envConfig.riotKey}`);
+const RIOT_API_URL_KR = 'https://kr.api.riotgames.com';
+const RIOT_API_URL_ASIA = 'https://asia.api.riotgames.com';
+const GET_summoner_by_name = (name: string): Promise<AxiosResponse<SummonerDto>> => {
+  return axiosRequest(
+    'GET',
+    `${RIOT_API_URL_KR}/lol/summoner/v4/summoners/by-name/${name}?api_key=${envConfig.riotKey}`
+  );
 };
+
+const GET_match_by_puuid = (puuid: string): Promise<AxiosResponse<SummonerDto>> => {
+  return axiosRequest(
+    'GET',
+    `${RIOT_API_URL_ASIA}/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=${envConfig.riotKey}`
+  );
+};
+
+export const RiotApi = {
+  GET_summoner_by_name,
+  GET_match_by_puuid,
+};
+// function Asia(target: any, key: string, desc: PropertyDescriptor): void {
+//   console.log(target);
+//   console.log(key);
+//   console.log(desc);
+
+//   const method = desc.value; // 기존의 method
+
+//   // 기존의 method가 error를 던졋을 때 error handling 할 수 있도록 재정의
+//   desc.value = function () {
+//     try {
+//       method(RIOT_API_URL_ASIA);
+//     } catch (err) {
+//       console.log('여기에 error handling logic 추가');
+//     }
+//   };
+// }
+
+// export class RiotApiModule {
+//   @Asia
+//   aaa(name: string) {
+//     const url = `/lol/summoner/v4/summoners/by-name/${name}`;
+//   }
+// }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import './MainPage.scss';
 import React, { useEffect } from 'react';
-import { GET_summoner_by_name } from 'src/api/riot';
+import { RiotApi } from 'src/api/riot';
 
 const MainPage: React.FC = () => {
   useEffect(() => {
@@ -8,9 +8,8 @@ const MainPage: React.FC = () => {
   }, []);
 
   const getSome = async () => {
-    const res = await GET_summoner_by_name('랜턴팔이%20서폿');
-
-    console.log(res.data.puuid);
+    const res = await RiotApi.GET_summoner_by_name('랜턴팔이%20서폿');
+    RiotApi.GET_match_by_puuid(res.data.puuid);
   };
 
   return <div className="main-page">Welcome To My Home Page!</div>;
